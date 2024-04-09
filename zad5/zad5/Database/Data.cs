5519b2379648274b919753c34b0193f61db6561b
Błażej Kujawiński <sxxxx@pjwstk.edu.pl>
2024-04-09 11:34:17 +0200

    Data fix

--- zad5/zad5/Database/Data.cs
+++ zad5/zad5/Database/Data.cs
@@ -4,10 +4,11 @@ namespace zad5.Database;
 
 public class Data
 {
-    public static List<Animal> Animals = new List<Animal>(
-        new Animal(1,"Reksio","Pies",10,"brown"),
-        new Animal(2,"Filemot","Kot",5,"white"),
-        new Animal(3,"Bonifacy","Kot",5,"black"),
-        new Animal(4,"Bogus","Niedzwiedz",100,"brown")
-    );
+    public static List<Animal> Animals = new List<Animal>
+    {
+        new Animal(1, "Reksio", "Pies", 10, "brown"),
+        new Animal(2, "Filemot", "Kot", 5, "white"),
+        new Animal(3, "Bonifacy", "Kot", 5, "black"),
+        new Animal(4, "Bogus", "Niedzwiedz", 100, "brown")
+    };
 }
