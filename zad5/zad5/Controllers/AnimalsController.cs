e2c6873b4a7e59184bb22fa7d9282fea66cd2166
Błażej Kujawiński <sxxxx@pjwstk.edu.pl>
2024-04-09 11:29:58 +0200

    init commit

---
+++ zad5/zad5/Controllers/AnimalsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace zad5.Controllers;
+
+[ApiController]
+[Route("/animals-controller")]
+//[Route("[controller]")]
+public class AnimalsController : ControllerBase
+{
+
+    
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult AddAnimals()
+    {
+        return Created();
+    }
+
+}
